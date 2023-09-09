5d6b8db7748c47b9d8a35479b7e93eb6be0b4ee4
Hizzy001 <hisrealh@gmail.com>
2023-09-08 22:09:41 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,18 +3,18 @@
 /**
 * main - prints all possible num
 *
-* Return: always 0.
+* Return: always 0
 */
 
 int main(void)
 {
 	int i;
 
-	for (i = 0 ; i < 10; i++) {
+	for (i = 0 ; i < 10 ; i++)
+	{
 		putchar(i + '0');
 		putchar(',');
 		putchar(' ');
 	}
 	return (0);
-
 }
