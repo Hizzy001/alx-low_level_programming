fe36ce9a804e9a1ee48526541e0ee4a86b8665b7
Hizzy001 <hisrealh@gmail.com>
2023-09-12 20:04:36 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,24 +6,20 @@
  */
 int main(void)
 {
-	int i = 0;
-	long j = 1, k = 2, fib1_half2;
+	int i;
+	long j = 0, k = 1, next;
 
-	while (i < 50)
-	{
-	if (i == 0)
-		printf("%ld", j);
-	else if (i == 1)
-		printf(", %ld", k);
+	for (i = 0; i < 50; i++)
+{
+		if (i < 49)
+		printf("%ld, ", j);
 	else
-	{
-		k += j;
-		j = k - j;
-		fib1_half2 = k % 10000000000;
-		printf(", %ld", fib1_half2);
+		printf("%ld\n", j);
+
+	next = j + k;
+	j = k;
+	k = next;
 	}
-	++i;
-	}
-	printf("\n");
+
 	return (0);
 }
