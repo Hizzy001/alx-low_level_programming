b2c4caeef8b92839c0c063b05dbdeb5d81e0569f
Hizzy001 <hisrealh@gmail.com>
2023-09-20 19:27:03 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,11 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
+#include <unistd.h>
 
+int _putchar(char c)
+{
+    return write(1, &c, 1);
+}
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);void print_numbers(void);
